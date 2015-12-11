25911c92382449d0579bc2a447349279bc63e347
颜风 <yanfengstyle@163.com>
2015-12-11 13:55:09 +0800

    添加MagicalRecord示例

--- MagicalRecord/MagicalRecord/ViewController.m
+++ MagicalRecord/MagicalRecord/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <MagicalRecord/MagicalRecord.h>
+#import "Person.h"
 
 @interface ViewController ()
 
@@ -17,6 +19,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIAlertView * alertView = [[UIAlertView alloc] initWithTitle:@"温馨提示" message:@"详见源文件ViewcControll.m" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [alertView show];
+    
+    /* 实体对象的创建保存与更新 */
+    // 获取上下文环境
+    NSManagedObjectContext *defaultContext = [NSManagedObjectContext MR_defaultContext];
+    
+    
+    // 在当前上下文环境中创建一个新的 Person 对象.
+    Person *person  = [Person MR_createEntityInContext:defaultContext];
+    person.firstname = @"firstname";
+    person.lastname  = @"lastname";
+    person.age       = @100;
+    
+    // 保存修改到当前上下文中.
+    [defaultContext MR_saveToPersistentStoreAndWait];
+    
+    
+    [MagicalRecord saveWithBlock:^(NSManagedObjectContext *localContext){
+        
+        Person *localPerson = [person MR_inContext:localContext];
+        localPerson.firstname = @"Yan";
+        localPerson.lastname = @"Feng";
+    }  completion:^(BOOL success, NSError *error) {
+        
+        NSArray * persons = [Person MR_findAllSortedBy:@"lastname:YES"
+                                                                     ascending:NO];
+        
+        [persons enumerateObjectsUsingBlock:^(Person * obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            NSLog(@"firstname: %@, lastname: %@\n", obj.firstname, obj.lastname);
+        }];
+        
+        
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
